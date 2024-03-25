7eb5337fd1adc11fdbdf7fbbad591fa3411ed16b
Vitor Diniz <vitor.diniz@ngosolucoes.com.br>
2024-03-25 17:21:18 -0300

    feat: estrategiaVitor criada

---
+++ estrategias/estrategiaVitor.mjs
@@ -0,0 +1,12 @@
+export function estrategiaVitor (ultimasJogadasDoOponente) {
+  // if(ultimasJogadasDoOponente.filter(valorEscolhido => valorEscolhido === true).length) {
+  //   if (Math.random <= 0.6){
+  //     return false
+  //   } else{
+  //     return true
+  //   }    
+  // } else{
+  //   return false
+  // }
+  return false
+}
